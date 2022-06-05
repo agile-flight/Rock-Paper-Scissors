3012a7c3c3ddc938275f96fd219c5a91d3edba42
Amartya Gunjan <53094015+agile-flight@users.noreply.github.com>
2022-06-05 12:45:25 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -17,17 +17,17 @@ int main() {
 
     for ( int i = 1; i <= 3; i++ ) {
 
-        int rand_num = (rand() % 3) + 1;
-
-        cout << "Trial #" << i << ": ";
-        cin >> ans;
-        transform(ans.begin(), ans.end(), ans.begin(), ::tolower);
+        int rand_num = (rand() % 3) + 1; // Generates A Random Integer So The Computer For Rock Paper Scissors
 
         /*
         1 : Rock
         2 : Paper 
         3 : Scissors
         */
+        
+        cout << "Trial #" << i << ": ";
+        cin >> ans;
+        transform(ans.begin(), ans.end(), ans.begin(), ::tolower);  // Changes The Input To Lowercase To Prepare It For Validation
 
         while ( ans != "rock" && ans != "paper" && ans != "scissors" ) {
 
@@ -37,7 +37,7 @@ int main() {
 
         }
 
-        if ( (ans == "rock" && rand_num == 3) || (ans == "paper" && rand_num == 1) || (ans == "scissors" && rand_num == 2) ) {
+        if ( (ans == "rock" && rand_num == 3) || (ans == "paper" && rand_num == 1) || (ans == "scissors" && rand_num == 2) ) {  // Stringy Conditional I Don't Like
             
             ++score;
             cout << "Round Won" << endl;
